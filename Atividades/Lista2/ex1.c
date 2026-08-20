6df5b36049a6a2d740bd12fa93e111cd4b2c7d25
Anne Caroline Moreno Gouvea <annecmgg@gmail.com>
2026-08-20 11:09:47 -0300

    Create ex1.c

---
+++ Atividades/Lista2/ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int idade, ano;
+	
+	printf("Qual sua idade ao fim do anos: ");
+	scanf("%d", &idade);
+	
+	printf("Qual ano voce esta: ");
+	scanf("%d", &ano);
+	
+	ano = ano - idade;
+	
+	printf("Voce nasceu no ano: %d", ano);
+	
+	return 0;
+	
+}
