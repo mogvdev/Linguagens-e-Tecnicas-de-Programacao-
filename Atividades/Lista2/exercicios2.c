8eed1131b7e8e8a7f3c30f79f4b789136badab21
Anne Caroline Moreno Gouvea <annecmgg@gmail.com>
2026-09-03 11:00:09 -0300

    Implement exercise selection and fix syntax issues
    Added options for multiple exercises and fixed syntax errors.

--- Atividades/Lista2/exercicios2.c
+++ Atividades/Lista2/exercicios2.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int op;
+printf("Insira qual exercício quer resolver" [1|2|3|8]\n");
+scanf("&d", &op);
+int op;
+
+
 //Exercício 1
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	
+Case 1;
+	/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+	int main(int argc, char *argv[]) {
 	int idade, ano;
-	
 	printf("Qual sua idade ao fim do anos: ");
 	scanf("%d", &idade);
-	
 	printf("Qual ano voce esta: ");
 	scanf("%d", &ano);
-	
 	ano = ano - idade;
-	
 	printf("Voce nasceu no ano: %d", ano);
+break;
 	
-	return 0;
 	
-}
+
+//Exercício 3
+
+Case 2;
+	// Faça um programa que oleia um valor em reais e a cotação do dólar. Em seguida, imprima o valor correspondente em dólares.
+	floar reais, cota;
+	printf("Insira a cotação e o valor: \n");
+	scanf("%f %f", &cota, &reais);
+	printf("Os %f reais sao %f trumps \n", reais, (reais/cota));
+break;
+
+//Exercício 4
+
+Case 3:
+	//Leia um valor que represente uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit
+	float tempC, tempF
+	printf("Insira a temperatura em C: \n");
+	scanf("%f", &tempC):
+	tempF = tempC * (9.0/5.0) + 32.0;
+	printf("Os %f C sao %f \n", tempC, tempF);
+break;
