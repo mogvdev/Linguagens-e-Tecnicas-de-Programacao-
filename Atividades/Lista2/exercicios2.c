cf248ad75c1fb75daeeba3d40b32ba5771a8badf
Anne Caroline Moreno Gouvea <annecmgg@gmail.com>
2026-09-03 11:04:45 -0300

    Update exercicios2.c

--- Atividades/Lista2/exercicios2.c
+++ Atividades/Lista2/exercicios2.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 int op;
-printf("Insira qual exercício quer resolver" [1|2|3|8]\n");
+printf("Insira qual exercício quer resolver" [1|3|4|10]\n");
 scanf("&d", &op);
 int op;
 
 
 //Exercício 1
 
-Case 1;
-	/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-	int main(int argc, char *argv[]) {
+case 1;
+	//Ano de nascença.
 	int idade, ano;
 	printf("Qual sua idade ao fim do anos: ");
 	scanf("%d", &idade);
@@ -21,11 +20,9 @@ Case 1;
 	printf("Voce nasceu no ano: %d", ano);
 break;
 	
-	
-
 //Exercício 3
 
-Case 2;
+case 3;
 	// Faça um programa que oleia um valor em reais e a cotação do dólar. Em seguida, imprima o valor correspondente em dólares.
 	floar reais, cota;
 	printf("Insira a cotação e o valor: \n");
@@ -35,11 +32,26 @@ break;
 
 //Exercício 4
 
-Case 3:
-	//Leia um valor que represente uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit
+case 4:
+	//Leia um valor que represente uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit.
 	float tempC, tempF
 	printf("Insira a temperatura em C: \n");
 	scanf("%f", &tempC):
 	tempF = tempC * (9.0/5.0) + 32.0;
 	printf("Os %f C sao %f \n", tempC, tempF);
 break;
+
+//Exercício 10
+
+case 10
+	//Comparação de valores.
+	int a,b,c,d, maior, maior_temp, maior2;
+	printf("Informe os valores a serem comparados: ");
+	scanf("%d %d %d %d", &a,&b,&c,&d);
+	//conta
+	maior_temp = (a+b+abs(a-b))/2;
+	maior2 = (d+c+abs(d-c))/2;
+	maior = (maior2+maior_temp+abs(maior2-maior_temp))/2;
+	printf("O maior entre |%d|%d|%d|%d| = %d", a,b,c,d, maior);
+break;
+
