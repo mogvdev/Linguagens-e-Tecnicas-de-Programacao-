a158ae5b47254590617294e5979e4c08922a6514
Anne Caroline Moreno Gouvea <annecmgg@gmail.com>
2026-09-03 19:31:32 -0300

    Delete Atividades/Lista2/ex8.c

--- Atividades/Lista2/ex8.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int sec, horas, min;
-printf("Insira o tempo em segundo \n");
-scanf("%d", &sec);
-horas = sec/3600;
-min = (sec - (sec%3600))/60;
-sec = sec - ((horas*3600)+(min*60);
-printf("\t %d:%d:%d", horas, min, sec);
