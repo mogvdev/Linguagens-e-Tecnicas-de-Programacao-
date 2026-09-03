b8d47bdab1e913046027c18c85caeb589a8e47a3
Anne Caroline Moreno Gouvea <annecmgg@gmail.com>
2026-09-03 19:28:33 -0300

    Update exercicios2.c

--- Atividades/Lista2/exercicios2.c
+++ Atividades/Lista2/exercicios2.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ex1(){
+	int idade, ano;
+	printf("Qual sua idade ao fim do anos: ");
+	scanf("%d", &idade);
+	printf("Qual ano voce esta: ");
+	scanf("%d", &ano);
+	ano = ano - idade;
+	printf("Voce nasceu no ano: %d", ano);
+}
+
 void ex2(){
 	float reais, cota;
 	printf("Insira a cotação e o valor: \n");
@@ -30,25 +40,28 @@ void ex8(){
 
 int main (int arc, char *argv[]){
 	int op;
-	printf("Insira qual exercício quer resolver [2|3|8]\n");
+	printf("Insira qual exercício quer resolver [1|2|3|8]\n");
 	scanf("%d", &op);
-    //Exercício 2
     switch(op){
-    case 2:
+    //Exercício 1
+		case 1:
+		ex1();
+		break;
+		
+	//Exercício 2
+		case 2:
     	ex2();
-    break;
+		break;
     	
     //Exercício 3
-    
-    case 3:
+    	case 3:
     	ex3();
-    break;
+    	break;
     
     //Exercício 8
-    
-    case 8:
+    	case 8:
     	ex8();
-    break;
+    	break;
     
     return 0;
     }
