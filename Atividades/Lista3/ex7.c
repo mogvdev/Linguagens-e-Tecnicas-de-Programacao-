aed2b2ed3999a32d4b9fb10b3fd931ec36c3ce9d
Anne Caroline Moreno Gouvea <annecmgg@gmail.com>
2026-09-10 11:33:16 -0300

    Update ex7.c

--- Atividades/Lista3/ex7.c
+++ Atividades/Lista3/ex7.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+float calc_inss (float salario){
+  if(salario<= 1412.00)return salario*0.075;
+  else if(salario<= 2666.68) return salario*0.09;
+  else if(salario<= 4000.00) return salario*0.12;
+  else return salario *0.14;
+}
+float calc_irpf (float salario){
+  if(salario<= 2259.20) return salario;
+  else if (salario<= 2826.65) return (salario*0.075) - 169.44;
+  else if (salario<= 3751.05) return (salario*0.15) - 381.44;
+  else if (salario<= 4664.68) return (salario*0.225) - 662.77;
+  else return (salario*0.275) - 896.00;
+}
+float valor_hora;{
+int vhora, thora, salario;
+  return salario = thora * vhora;
+}
+
+
+int main(){
+  float salario, desconto, desconto1, irpf;
+  desconto = calc_inss(salario);
+  desconto1 = calc_irpf(salario);
+  irpf = desconto + desconto1;
+  printf("======================================================\n");
+  printf("    RECIBO DE PAGAMENTO DE SALÁRIO (CONTRA-CHEQUE)    \n");
+  printf("======================================================\n");
+  printf("Salário Bruto (Horas x Valor):   R$%f\n", valor_hora(salario));
+  printf(" (-) Desconto INSS:               R$%f\n", calc_inss(salario));
+  printf(" (-) Desconto IRPF:               R$%f\n", calc_irpf(salario));
+  printf("------------------------------------------------------\n");
+  printf("LÍQUIDO A RECEBER: \n", irpf);
+  printf("======================================================\n");
+    
+    return 0;
+}
